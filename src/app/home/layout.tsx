f04c2b51081858e8f167321f80a5d76482ccd0ac
Sohail Ali <alisohai889@gmail.com>
2025-05-25 08:39:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SiteFooter>
    
    with the text content:
    
        © 2025 Gastronomic Getaway. All rights reserved.
    
    Relevant files:
    
        - https://6000-firebase-studio-1747800815793.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/node_modules_a31e2e15._.js
        - https://6000-firebase-studio-1747800815793.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1747800815793.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    why is it showing inside the food item card

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -9,8 +9,8 @@ export default function HomeLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col min-h-screen">
       <SiteHeader />
-      {/* This div controls the main content and footer alignment and width */}
-      <div className="flex flex-1 flex-col w-full max-w-5xl ml-auto"> 
+      {/* This div controls the main content and footer alignment and width. Added overflow-hidden. */}
+      <div className="flex flex-1 flex-col w-full max-w-5xl ml-auto overflow-hidden">
         <main className="flex-grow overflow-y-auto"> {/* Ensure main content can scroll */}
           {children}
         </main>
